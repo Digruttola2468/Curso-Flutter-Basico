915d4a9ed0d0429494df63c082c14daff244ef01
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-10-20 18:57:02 -0300

    Tipografías y Google Fonts

--- README.md
+++ README.md
@@ -207,3 +207,26 @@ Es recomendable que al momento de analizar el layout de una interfaz ya diseñad
 [Layout widgets - Flutter](https://docs.flutter.dev/development/ui/widgets/layout)
 
 [UI7 App - Uplabs](https://www.uplabs.com/posts/ui7-kit)
+
+## Tipografias y Google Fonts
+
+Tipografías nos referimos precisamente al tipo y estilo de letras que usaremos en los textos de nuestra interfaz, esta característica estará determinada por las indicaciones que nos haya suministrado nuestro equipo de diseño.
+
+Un sitio web muy utilizado e interesante para obtener tipografías que puedes usar en tus proyectos es Google Fonts.
+
+Para incorporar las tipografías personalizadas a nuestro proyecto crearemos una carpeta particular llamada fonts/ en la que guardaremos los archivos que descarguemos desde Google Fonts. Seguidamente, debemos declarar este recurso en el archivo pubspecs.yaml. Este archivo contiene las especificaciones de todos los recursos públicos que estarán disponibles para ser usados en nuestro proyecto. La sintaxis yaml está inspirada en json y xml y se usa en Flutter para definir la estructura de recursos y configuraciones globales de nuestra aplicación.
+
+La forma de declarar los recursos de tipografía que usaremos en nuestro proyecto es la siguiente:
+
+```YAML
+...
+fonts:
+  - family:
+  fonts:
+    - asset:
+...
+```
+Algo que se debe tener en cuenta es que yaml es bastante estricto con la indentación de los datos. En otras palabras, la sangría, tabulación o espaciado de cada línea debe estar indicada correctamente para que no haya problemas con la lectura y procesamiento posterior de las configuraciones.
+
+Para aplicar la nueva tipografía a nuestro código bastará con incluir en el widget TextStyle, la propiedad fontFamily e indicar en ella el nombre de la fuente que hemos declarado en nuestro archivo de configuración.
+
---
+++ lib/description_place.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class DescripcionPlace extends StatelessWidget {
+
+  String namePlace ;
+  String descriptionString;
+  int starts = 1;
+
+  DescripcionPlace(this.namePlace,this.starts,this.descriptionString, {super.key} );
+
+  @override
+  Widget build(BuildContext context) {
+
+    final start_half = Container(
+      margin: const EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: const Icon(
+        Icons.star_half,
+        color: Color(0xFFF2C611)
+      ),
+    );
+
+    final star_border = Container(
+      margin: const EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+      child: const Icon(
+          Icons.star_border,
+          color: Color(0xFFF2C611)
+      ),
+    );
+
+    final start = Container(
+      margin: const EdgeInsets.only(
+        top: 223.0,
+        right: 3.0
+      ),
+      child: const Icon(
+        Icons.star,
+        color: Color(0xFFF2C611),
+      ),
+    );
+
+    final title_stars = Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(
+            top: 220.0,
+            left: 20.0,
+            right: 20.0
+          ),
+          child: Text(
+            namePlace,
+            style: const TextStyle(
+              fontFamily: "Lato",
+              fontSize: 30.0,
+              fontWeight: FontWeight.w900
+            ),
+          textAlign: TextAlign.left,
+          ),
+        ),
+        Row(
+          children: [
+            start,start,start,start,start
+          ],
+        )
+      ],
+    );
+
+    final description = Container(
+      margin: const EdgeInsets.only(
+        top: 20,
+        left: 20,
+        right: 20
+      ),
+      child: Text(
+        descriptionString,
+        style: const TextStyle(
+            fontFamily: "Lato",
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a)
+        ),
+      ),
+    );
+
+    return Column(
+      children: [
+        title_stars,
+        description
+      ],
+    );
+  }
+}
